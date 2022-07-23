4a0aedb9e9ff593bbeee8a07fb0bfe6acf3a76b1
meteorstrike77 <89230784+meteorstrike77@users.noreply.github.com>
2022-07-23 11:57:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(630, 640);
@@ -6,6 +7,14 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(480, 380);
     video.hide();
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() {
@@ -19,4 +28,33 @@ function modelLoaded() {
 }
 function draw() {
     image(video, 0, 0, 630, 630);
+        if (status != "") {
+        objectDetector.detect(video, gotResult);
+         for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == input) {
+
+                document.getElementById("objectfound").innerHTML = "Object Found";
+                videoLiveView.stop();
+                objectDetector.detect(gotResult);
+            
+
+                var synth = window.speechSynthesis;
+                speak_data = "Object Mentioned Found";
+                var utterThis = new SpeechSynthesisUtterance(speak_data);
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("objectfound").innerHTML = "Object Not Found";
+            }
+        }
+    }
 }
